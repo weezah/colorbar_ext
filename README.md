9f225394bbc336f00831346578b847cf3edad6bc
Mattia <mattia.ryffel@disneyresearch.com>
2024-02-27 12:34:20 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# ColorBar Extension    
+
+A small extension that customizes the colors of vscode for every workspace
+
---
+++ extension.js
@@ -0,0 +1,116 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+const defaultSettings = {
+	"colors": {
+		"Navy Blue": "#000080",
+		"Indigo Blue": "#3f00ff",
+		"Eden Blue": "#0d5a56",
+		"Teal": "#48aaad",
+		"Torch Red": "#ce173b",
+		"RedWood": "#5a200d",
+		"Sepia": "#700214",
+		"Grape": "#663046",
+		"Bronze Olive": "#42490b",
+		"San Felix": "#1b5506",
+		"Avocado": "#568203",
+		"Dijon": "#c49102",
+		"Wolf": "#504a4b",
+		"Wood": "#3f301d",
+	}
+};
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// add colors to the settings
+	let colorbarConfig = vscode.workspace.getConfiguration();
+	let currentColors = colorbarConfig.get('colorbar.colors') || {};
+	let newSettings = {...defaultSettings['colors'], ...currentColors};
+	colorbarConfig.update("colorbar", newSettings, vscode.ConfigurationTarget.Global);
+
+	let jsonData = colorbarConfig.get("colorbar.colors")
+	let keys = Object.keys(jsonData);
+
+	function updateColorCustomizations(selection) {
+
+		let configuration = vscode.workspace.getConfiguration();
+
+		let color = jsonData[selection];
+		let colorConfig = {
+			"activityBar.background": color,
+			"activityBar.foreground": pSBC(0.9, color),
+			"activityBar.activeBackground": pSBC(0.13, color),
+			"activityBar.inactiveForeground": pSBC(0.66, color),
+			"titleBar.activeBackground": pSBC(-0.1, color),
+			"statusBar.background": color,
+			"statusBar.foreground": pSBC(0.9, color)
+		};
+
+		configuration.update("workbench.colorCustomizations", colorConfig, vscode.ConfigurationTarget.Workspace);
+	}
+
+	let colorPicker = vscode.commands.registerCommand('colorbar.pick', () => {
+
+		vscode.window.showQuickPick(keys, {
+			onDidSelectItem: item => {
+				updateColorCustomizations(item);
+			}
+		}).then(selection => {
+
+			if (!selection) {
+				return;
+			}
+			updateColorCustomizations(selection);
+
+		});
+	});
+
+	let editColors = vscode.commands.registerCommand('colorbar.edit', () => {
+		vscode.commands.executeCommand('workbench.action.openSettings', '@ext:weezah.colorbar');
+	});
+
+
+	context.subscriptions.push(colorPicker, editColors);
+}
+
+function deactivate() { }
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	activate,
+	deactivate
+}
+
+// Credits to https://github.com/PimpTrizkit/PJs/
+const pSBC = (p, c0, c1, l) => {
+	let r, g, b, P, f, t, h, m = Math.round, a = typeof (c1) == "string";
+	if (typeof (p) != "number" || p < -1 || p > 1 || typeof (c0) != "string" || (c0[0] != 'r' && c0[0] != '#') || (c1 && !a)) return null;
+	h = c0.length > 9, h = a ? c1.length > 9 ? true : c1 == "c" ? !h : false : h, f = pSBC.pSBCr(c0), P = p < 0, t = c1 && c1 != "c" ? pSBC.pSBCr(c1) : P ? { r: 0, g: 0, b: 0, a: -1 } : { r: 255, g: 255, b: 255, a: -1 }, p = P ? p * -1 : p, P = 1 - p;
+	if (!f || !t) return null;
+	if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
+	else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
+	a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
+	if (h) return "rgb" + (f ? "a(" : "(") + r + "," + g + "," + b + (f ? "," + m(a * 1000) / 1000 : "") + ")";
+	else return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2)
+}
+
+pSBC.pSBCr = (d) => {
+	const i = parseInt;
+	let n = d.length, x = {};
+	if (n > 9) {
+		const [r, g, b, a] = (d = d.split(','));
+		n = d.length;
+		if (n < 3 || n > 4) return null;
+		x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4)), x.g = i(g), x.b = i(b), x.a = a ? parseFloat(a) : -1
+	} else {
+		if (n == 8 || n == 6 || n < 4) return null;
+		if (n < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : "");
+		d = i(d.slice(1), 16);
+		if (n == 9 || n == 5) x.r = d >> 24 & 255, x.g = d >> 16 & 255, x.b = d >> 8 & 255, x.a = Math.round((d & 255) / 0.255) / 1000;
+		else x.r = d >> 16, x.g = d >> 8 & 255, x.b = d & 255, x.a = -1
+	} return x
+};
